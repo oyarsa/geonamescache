58246aefe78a4a3dfb1437d1a05309306807eb66
Italo Silva <italo@maleldil.com>
2020-11-20 14:54:17 -0300

    Update continents file and script
    Adds proper error handling to the script. Error checking needs to happen
    _before_ the resp.ok test, not inside it.

--- scripts/continents.py
+++ scripts/continents.py
@@ -2,34 +2,40 @@
 # -*- coding: utf-8 -*-
 # Get continent data oincluding bounding boxes and centroids for and store it
 # in json file keyed by continentCode.
-import os
 import json
+import os
+import sys
+from typing import Any, Dict
+
 import requests
 
 # see http://download.geonames.org/export/dump/readme.txt
 continent_ids = [6255146, 6255147, 6255148, 6255149, 6255151, 6255150, 6255152]
-url = 'http://api.geonames.org/getJSON'
+url = "http://api.geonames.org/getJSON"
 params = {
-    'formatted': 'true',
-    'username': os.environ['GEONAMES_USER'],
-    'geonameId': None
+    "formatted": "true",
+    "username": os.environ["GEONAMES_USER"],
+    "geonameId": None,
 }
 continents = {}
 
 
-def account_ok(j):
-    if j.get(u'status', {}).get(u'value') == 10:
-        print(j[u'status'][u'message'])
-        exit(1)
+def account_ok(j: Dict[str, Any]) -> None:
+    if j.get(u"status", {}).get(u"value") == 10:
+        print(j[u"status"][u"message"])
+        sys.exit(1)
 
 
 for geoid in continent_ids:
-    params['geonameId'] = geoid
+    params["geonameId"] = geoid
     resp = requests.get(url, params=params)
-    if resp.ok:
-        cont = json.loads(resp.text)
-        account_ok(cont)
-        continents[cont['continentCode']] = cont
 
-with open('geonamescache/continents.json', 'w') as f:
+    cont = json.loads(resp.text)
+    account_ok(cont)
+
+    if resp.ok:
+        continents[cont["continentCode"]] = cont
+
+
+with open("geonamescache/continents.json", "w") as f:
     json.dump(continents, f)
