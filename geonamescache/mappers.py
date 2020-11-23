b9db4d3a7cff45d9dc583988ca407f8d83688f53
Italo Silva <italo@maleldil.com>
2020-11-23 10:32:29 -0300

    Add case-insensitive options to some methods

--- geonamescache/mappers.py
+++ geonamescache/mappers.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
+from typing import Callable, Dict, Optional
+
 from geonamescache import GeonamesCache
+
 from . import mappings
 
 
-def country(from_key='name', to_key='iso'):
+def country(
+    from_key: str = "name", to_key: str = "iso", case: bool = True
+) -> Callable[[str], Optional[str]]:
     """Creates and returns a mapper function to access country data.
 
     The mapper function that is returned must be called with one argument. In
@@ -14,20 +19,31 @@ def country(from_key='name', to_key='iso'):
         Defaults to ``name``.
     :param to_key: (optional) the country attribute you want as output.
         Defaults to ``iso``.
+    :param case: (optional) whether the map input is case sensitive.
+        Defaults to True.
     :return: mapper
     :rtype: function
     """
 
     gc = GeonamesCache()
-    dataset = gc.get_dataset_by_key(gc.get_countries(), from_key)
+    dataset: Dict[str, Dict[str, str]] = gc.get_dataset_by_key(
+        gc.get_countries(), from_key
+    )
 
-    def mapper(input):
+    if not case:
+        i_dataset = {key.lower(): value for key, value in dataset.items()}
+        dataset.update(i_dataset)
+
+    def mapper(input: str) -> Optional[str]:
         # For country name inputs take the names mapping into account.
-        if 'name' == from_key:
+        if not case:
+            input = input.lower()
+        if "name" == from_key:
             input = mappings.country_names.get(input, input)
         # If there is a record return the demanded attribute.
         item = dataset.get(input)
         if item:
             return item[to_key]
+        return None
 
     return mapper
--- scripts/countries2csv.py
+++ scripts/countries2csv.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import pandas as pd
+
 from geonamescache import GeonamesCache
 
 gc = GeonamesCache()
 countries = gc.get_countries()
-df = pd.DataFrame.from_dict(countries, orient='index')
-df.to_csv('geonamescache/countries.csv', index=False)
+df = pd.DataFrame.from_dict(countries, orient="index")
+df.to_csv("geonamescache/countries.csv", index=False)
